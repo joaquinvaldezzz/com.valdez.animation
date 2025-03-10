5dc5c48d4b853698863db33ac17848b6cc1d6baf
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-10 11:24:01 +0800

    feat: create rotate animation

--- app/src/main/java/com/valdez/animation/MainActivity.kt
+++ app/src/main/java/com/valdez/animation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.valdez.animation
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
@@ -12,5 +13,10 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_design)
     }
 
-    fun rotate(view: View) {}
+    fun rotate() {
+        val image = findViewById<View>(R.id.image_dice)
+        val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.rotate)
+
+        image.startAnimation(animation)
+    }
 }

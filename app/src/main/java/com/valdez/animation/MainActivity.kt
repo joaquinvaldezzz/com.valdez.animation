3afee503afcaf85ddfce55ba4fa1a3df41b6ce1a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-10 12:10:04 +0800

    feat: create more animations
    - feat: create counterclockwise animation
    - feat: create slide left animation
    - feat: create slide right animation

--- app/src/main/java/com/valdez/animation/MainActivity.kt
+++ app/src/main/java/com/valdez/animation/MainActivity.kt
@@ -13,9 +13,17 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_design)
     }
 
-    fun rotate(view: View) {
+    fun rotateClockwise(view: View) {
         val image = findViewById<View>(R.id.image_dice)
-        val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.rotate)
+        val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_clockwise)
+
+        image.startAnimation(animation)
+    }
+
+    fun rotateCounterclockwise(view: View) {
+        val image = findViewById<View>(R.id.image_dice)
+        val animation =
+            AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_counterclockwise)
 
         image.startAnimation(animation)
     }
@@ -30,6 +38,20 @@ class MainActivity : ComponentActivity() {
     fun zoomOut(view: View) {
         val image = findViewById<View>(R.id.image_dice)
         val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_out)
+
+        image.startAnimation(animation)
+    }
+
+    fun slideLeft(view: View) {
+        val image = findViewById<View>(R.id.image_dice)
+        val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_left)
+
+        image.startAnimation(animation)
+    }
+
+    fun slideRight(view: View) {
+        val image = findViewById<View>(R.id.image_dice)
+        val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_right)
 
         image.startAnimation(animation)
     }
